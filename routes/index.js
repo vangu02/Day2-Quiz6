b9d7e4d8c58e2901cfeea550a33e85a4d8738e58
vangu02 <guy.vanroye@gmail.com>
2015-03-17 16:59:53 -0700

    Quiz 7 day2 assigment added

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,20 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+/* PUT response. */
+router.put('/put', function(req, res, next) {
+    res.send('PUT has been requested');
+});
+
+/* POST response. */
+router.post('/post', function(req, res, next) {
+    res.send('POST has been requested');
+});
+
+/* DELETE response. */
+router.delete('/delete', function(req, res, next) {
+    res.send('DELETE has been requested');
+});
+
 module.exports = router;
